c00b51530888f9d3309b171844dd5fbe32701605
Raphael Passos <raphael251@hotmail.com>
2019-03-26 13:49:40 -0300

    Add the mongodb strategy

--- 09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
+++ 09-multi-db-postgres-mongodb/src/db/strategies/mongodb.js
@@ -1,12 +1,66 @@
 const ICrud = require('./interfaces/interfaceCrud')
+const Mongoose = require('mongoose')
+const STATUS = {
+    0: 'Desconectado',
+    1: 'Conectado',
+    2: 'Conectando',
+    3: 'Desconectando'
+}
 
 class MongoDB extends ICrud {
     constructor() {
         super()
+        this._herois = null
+        this._driver = null
     }
+    async isConnected() {
+        const state = STATUS[this._driver.readyState];
 
+        if (state === 'Conectando')
+            await new Promise(resolve => setTimeout(resolve, 1000))
+
+        return STATUS[this._driver.readyState];
+    }
+    defineModel() {
+        const heroiSchema = new Mongoose.Schema({
+            nome: {
+                type: String,
+                required: true,
+            },
+            poder: {
+                type: String,
+                required: true
+            },
+            insertedAt: {
+                type: Date,
+                default: new Date()
+            }
+        })
+        this._herois = Mongoose.model('herois', heroiSchema)
+    }
+    connect() {
+        Mongoose.connect('mongodb://raphaelpassos:underthebridge@localhost:27017/herois', {
+            useNewUrlParser: true
+        }, function (error) {
+            if (!error) return;
+            console.log('Falha na conexao', error)
+        })
+        const connection = Mongoose.connection
+        this._driver = connection
+        connection.once('open', () => console.log('database rodando!'))
+        this.defineModel()
+    }
     create(item) {
-        console.log('O item foi salvo em MongoDB')
+        return this._herois.create(item)
+    }
+    read(item, skip=0, limit=10) {
+        return this._herois.find(item).skip(skip).limit(limit)
+    }
+    update(id, item) {
+        return this._herois.updateOne({ _id: id }, { $set: item })
+    }
+    delete(id) {
+        return this._herois.deleteOne({ _id: id })
     }
 }
 
