706b57358e44df0b70cc8d1db3011d605ecaf679
Raphael Passos <raphael251@hotmail.com>
2019-03-28 13:23:47 -0300

    Add the create feature in API

--- 16-hapi-listar-joi/src/routes/heroRoutes.js
+++ 16-hapi-listar-joi/src/routes/heroRoutes.js
@@ -1,6 +1,8 @@
 const BaseRoute = require('./base/baseRoute')
 const Joi = require('joi')
-
+const failAction = (request, headers, erro) => {
+    throw erro;
+}
 class HeroRoutes extends BaseRoute {
     constructor(db) {
         super()
@@ -17,9 +19,7 @@ class HeroRoutes extends BaseRoute {
                     // headers -> header
                     // params -> na URL :id
                     // query -> ?skip=00&limit=10
-                    failAction: (request, headers, erro) => {
-                        throw erro;
-                    },
+                    failAction,
                     query: {
                         skip: Joi.number().integer().default(0),
                         limit: Joi.number().integer().default(10),
@@ -45,6 +45,42 @@ class HeroRoutes extends BaseRoute {
                 } catch (error) {
                     console.log('DEU RUIM', error)
                     return "ERRO INTERNO NO SERVIDOR"
+                }
+            }
+        }
+    }
+
+    create() {
+        return {
+            path: '/herois',
+            method: 'POST',
+            config: {
+                validate: {
+                    failAction,
+                    payload: {
+                        nome: Joi.string().required().min(3).max(100),
+                        poder: Joi.string().required().min(2).max(100)
+                    }
+                }
+            },
+            handler: async (request) => {
+                try {
+                    const {
+                        nome,
+                        poder
+                    } = request.payload
+                    const result = await this.db.create({
+                        nome,
+                        poder
+                    })
+
+                    return {
+                        message: 'Heroi cadastrado com sucesso!',
+                        _id: result._id
+                    }
+                } catch (error) {
+                    console.log('DEU RUIM', error)
+                    return "Internal Error!"
                 }
             }
         }
