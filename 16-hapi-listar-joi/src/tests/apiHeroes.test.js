b6bba98c23130bf7d888aff7c2331d9c932c3a94
Raphael Passos <raphael251@hotmail.com>
2019-03-28 13:25:41 -0300

    Add /herois POST test

--- 16-hapi-listar-joi/src/tests/apiHeroes.test.js
+++ 16-hapi-listar-joi/src/tests/apiHeroes.test.js
@@ -2,12 +2,17 @@ const assert = require('assert')
 const api = require('./../api')
 let app = {}
 
-describe('Suite de testes da API Heroes', function () {
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Chapolin Colorado',
+    poder: 'Marreta Bionica'
+}
+
+describe.only('Suite de testes da API Heroes', function () {
     this.beforeAll(async () => {
         app = await api
     })
 
-    it('Listar na /herois', async () => {
+    it('Listar GET - /herois', async () => {
         const result = await app.inject({
             method: 'GET',
             url: '/herois?skip=0&limit=0'
@@ -19,7 +24,7 @@ describe('Suite de testes da API Heroes', function () {
         assert.ok(Array.isArray(dados))
     })
 
-    it('Listar na /herois - Deve retornar somente 3 registros', async () => {
+    it('Listar GET - /herois - Deve retornar somente 3 registros', async () => {
         const TAMANHO_LIMITE = 3
         const result = await app.inject({
             method: 'GET',
@@ -33,7 +38,7 @@ describe('Suite de testes da API Heroes', function () {
         assert.ok(dados.length === TAMANHO_LIMITE)
     })
 
-    it('Listar na /herois - Deve retornar um erro com limit incorreto', async () => {
+    it('Listar GET - /herois - Deve retornar um erro com limit incorreto', async () => {
         const TAMANHO_LIMITE = 'AEEwE'
         const result = await app.inject({
             method: 'GET',
@@ -55,7 +60,7 @@ describe('Suite de testes da API Heroes', function () {
         assert.deepEqual(dados, errorResult)
     })
 
-    it('Listar na /herois - Deve filtrar um item', async () => {
+    it('Listar GET - /herois - Deve filtrar um item', async () => {
         const NAME = 'Homem Aranha-1553616093464'
         const result = await app.inject({
             method: 'GET',
@@ -68,4 +73,18 @@ describe('Suite de testes da API Heroes', function () {
         assert.deepEqual(statusCode, 200)
         assert.deepEqual(dados[0].nome, NAME)
     })
+
+    it('Cadastrar POST - /herois', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: `/herois`,
+            payload: MOCK_HEROI_CADASTRAR
+        })
+        
+        const statusCode = result.statusCode
+        const { message } = JSON.parse(result.payload)
+        assert.deepEqual(message, "Heroi cadastrado com sucesso!")
+        assert.notStrictEqual(_id, undefined)
+        assert.ok(statusCode === 200)
+   })
 })
