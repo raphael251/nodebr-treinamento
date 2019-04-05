e769ea8acd331da5c748d495fe0c2d5910012d05
Raphael Passos <raphael251@hotmail.com>
2019-04-05 16:02:14 -0300

    Add the authHeroes route test

--- 21-hapi-user-validation/src/tests/authHeroes.test.js
+++ 21-hapi-user-validation/src/tests/authHeroes.test.js
@@ -1,27 +1,63 @@
 const assert = require('assert')
 const api = require('./../api')
+const Context = require('./../db/strategies/base/contextStrategy')
+const Postgres = require('./../db/strategies/postgres/postgres')
+const UsuarioSchema = require('./../db/strategies/postgres/schemas/usuarioSchema')
+
 let app = {}
+
+const USER = {
+    username: 'xuxadasilva',
+    password: '123'
+}
+
+const USER_DB = {
+    username: USER.username.toLowerCase(),
+    password: '$2b$04$TuUpqIRfW2LHycZqGOeQbOJWS1FS4SKxuEdi3Dq7pzekMooc0wACa'
+}
+
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Inh1eGFkYXNpbHZhIiwiaWQiOjEsImlhdCI6MTU1NDMyOTk0OX0.PdbW1hWRK3wFJLtfaCJQkXq7-WbMNjfPRAMldzFWAyY"
 
 describe('Auth test suite', function () {
     this.beforeAll(async () => {
         app = await api
+
+        const connectionPostgres = await Postgres.connect()
+        const model = await Postgres.defineModel(connectionPostgres, UsuarioSchema)
+        const contextPostgres = new Context(new Postgres(connectionPostgres, model))
+        await contextPostgres.update(null, USER_DB, true)
     })
 
-    it('deve obter um token', async () => { 
+    it('Deve obter um token', async () => {
         const result = await app.inject({
             method: 'POST',
             url: '/login',
-            payload: {
-                username: 'xuxadasilva',
-                password: '123'
-            }
+            payload: USER
         })
 
-        const { statusCode } = result
+        const {
+            statusCode
+        } = result
         const dados = JSON.parse(result.payload)
 
         assert.deepEqual(statusCode, 200)
         assert.ok(dados.token.length > 10)
+    })
+
+    it('Deve retornar não autorizado ao tentar obter um login errado', async () => {
+        const result = await app.inject({
+            method: 'POST',
+            url: '/login',
+            payload: {
+                username: 'raphaelpassos',
+                password: '123'
+            }
+        })
+        const { statusCode } = result
+        const dados = JSON.parse(result.payload)
+
+        assert.deepEqual(statusCode, 401)
+        assert.deepEqual(dados.error, 'Unauthorized')
+
     })
 })
