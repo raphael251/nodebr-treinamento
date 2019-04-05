f0280ded7cffaea54a0d494bdc9b3617d21e6f45
Raphael Passos <raphael251@hotmail.com>
2019-04-05 16:01:44 -0300

    Add the /login route to authenticate the users

--- 21-hapi-user-validation/src/routes/authRoutes.js
+++ 21-hapi-user-validation/src/routes/authRoutes.js
@@ -4,7 +4,7 @@ const Boom = require('boom')
 // npm i jsonwebtoken
 // npm i hapi-auth-jwt2
 const Jwt = require('jsonwebtoken')
-
+const PasswordHelper = require('./../helpers/passwordHelper')
 
 const failAction = (request, headers, erro) => {
     throw erro;
@@ -15,9 +15,10 @@ const USER = {
 }
 
 class AuthRoutes extends BaseRoute {
-    constructor(secret) {
+    constructor(secret, db) {
         super()
         this.secret = secret
+        this.db = db
     }
     login() {
         return {
@@ -41,16 +42,30 @@ class AuthRoutes extends BaseRoute {
                     username,
                     password
                 } = request.payload
+                const [ usuario ] = await this.db.read({
+                    username: username.toLowerCase()
+                })
 
-                if (
-                    username.toLowerCase() !== USER.username ||
-                    password !== USER.password
-                )
-                    return Boom.unauthorized()
+                if (!usuario) {
+                    return Boom.unauthorized('O usuario informado não existe!')
+                }
+
+                const match = await PasswordHelper
+                    .comparePassword(password, usuario.password)
+
+                if (!match) {
+                    return Boom.unauthorized('Usuário ou senha inválidos!')
+                }
+
+                // if (
+                //     username.toLowerCase() !== USER.username ||
+                //     password !== USER.password
+                // )
+                //     return Boom.unauthorized()
 
                 const token = Jwt.sign({
                     username: username,
-                    id: 1
+                    id: usuario.id
                 }, this.secret)
 
                 return {
