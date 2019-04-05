0bd1566eb550fcca22d916ea4c1b583246900ad3
Raphael Passos <raphael251@hotmail.com>
2019-04-05 19:36:38 -0300

    Add the process.env configs

--- 22-env/src/api.js
+++ 22-env/src/api.js
@@ -3,47 +3,70 @@
 // npm i hapi-auth-jwt2
 // npm i bcrypt
 
-const Hapi = require('hapi')
-const Context = require('./db/strategies/base/contextStrategy')
-const MongoDb = require('./db/strategies/mongodb/mongodb')
-const HeroiSchema = require('./db/strategies/mongodb/schemas/heroiSchema')
-const HeroRoute = require('./routes/heroRoutes')
-const AuthRoute = require('./routes/authRoutes')
+const {
+    config
+} = require('dotenv')
+const {
+    join
+} = require('path')
+const {
+    ok
+} = require('assert')
 
-const Postgress = require('./db/strategies/postgres/postgres')
-const UsuarioSchema = require('./db/strategies/postgres/schemas/usuarioSchema')
+const env = process.env.NODE_ENV || "dev"
+ok(env === "dev" || env === "prod", "a env é inválida, ou dev ou prod")
 
-const HapiSwagger = require('hapi-swagger')
-const Vision = require('vision')
-const Inert = require('inert')
-
-const HapiJwt = require('hapi-auth-jwt2')
-const JWT_SECRET = 'MEU_SEGREDÃO_123'
-
-const app = new Hapi.Server({
-    port: 5001
+const configPath = join(__dirname, './config', `.env.${env}`)
+config({
+    path: configPath
 })
 
+const Hapi = require("hapi");
+const Context = require("./db/strategies/base/contextStrategy");
+const MongoDb = require("./db/strategies/mongodb/mongodb");
+const HeroiSchema = require("./db/strategies/mongodb/schemas/heroiSchema");
+const HeroRoute = require("./routes/heroRoutes");
+const AuthRoute = require("./routes/authRoutes");
+
+const Postgress = require("./db/strategies/postgres/postgres");
+const UsuarioSchema = require("./db/strategies/postgres/schemas/usuarioSchema");
+
+const HapiSwagger = require("hapi-swagger");
+const Vision = require("vision");
+const Inert = require("inert");
+
+const HapiJwt = require("hapi-auth-jwt2");
+const JWT_SECRET = process.env.JWT_KEY;
+
+const app = new Hapi.Server({
+    port: process.env.PORT
+});
+
 function mapRoutes(instance, methods) {
-    return methods.map(method => instance[method]())
+    return methods.map(method => instance[method]());
 }
 
 async function main() {
-    const connection = MongoDb.connect()
-    const context = new Context(new MongoDb(connection, HeroiSchema))
-    
-    const connectionPostgres = await Postgress.connect()
-    const usuarioSchema = await Postgress.defineModel(connectionPostgres, UsuarioSchema)
-    const contextPostgress = new Context(new Postgress(connectionPostgres, usuarioSchema))
+    const connection = MongoDb.connect();
+    const context = new Context(new MongoDb(connection, HeroiSchema));
+
+    const connectionPostgres = await Postgress.connect();
+    const usuarioSchema = await Postgress.defineModel(
+        connectionPostgres,
+        UsuarioSchema
+    );
+    const contextPostgress = new Context(
+        new Postgress(connectionPostgres, usuarioSchema)
+    );
 
     const swaggerOptions = {
         info: {
-            title: 'API Herois - #CursoNodeBR',
-            version: 'v1.0'
+            title: "API Herois - #CursoNodeBR",
+            version: "v1.0"
         },
-        lang: 'pt'
-    }
-    
+        lang: "pt"
+    };
+
     await app.register([
         HapiJwt,
         Vision,
@@ -52,9 +75,9 @@ async function main() {
             plugin: HapiSwagger,
             options: swaggerOptions
         }
-    ])
+    ]);
 
-    app.auth.strategy('jwt', 'jwt', {
+    app.auth.strategy("jwt", "jwt", {
         key: JWT_SECRET,
         // options: {
         //     expiresIn: 20
@@ -64,32 +87,34 @@ async function main() {
             //verifica no banco se o usuario continua pagando
             const [result] = await contextPostgress.read({
                 username: dado.username.toLowerCase()
-            })
+            });
 
             if (!result) {
                 return {
                     isValid: false
-                }
+                };
             }
 
             return {
                 isValid: true
-            }
+            };
         }
-    })
+    });
 
-    app.auth.default('jwt')
+    app.auth.default("jwt");
 
     app.route([
         ...mapRoutes(new HeroRoute(context), HeroRoute.methods()),
-        ...mapRoutes(new AuthRoute(JWT_SECRET, contextPostgress), AuthRoute.methods())
-    ]
-    )
+        ...mapRoutes(
+            new AuthRoute(JWT_SECRET, contextPostgress),
+            AuthRoute.methods()
+        )
+    ]);
 
-    await app.start()
-    console.log('Servidor rodando na porta', app.info.port)
+    await app.start();
+    console.log("Servidor rodando na porta", app.info.port);
 
-    return app
+    return app;
 }
 
-module.exports = main()
+module.exports = main();
