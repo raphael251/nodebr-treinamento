ba4b66bc039aae38dc2dbe562addc77927da0785
Raphael Passos <raphael251@hotmail.com>
2019-04-08 13:15:40 -0300

    Add the 24-pm2 folder

---
+++ 24-heroku/src/db/strategies/postgres/postgres.js
@@ -0,0 +1,67 @@
+const ICrud = require('./../interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
+
+class Postgres extends ICrud {
+    constructor(connection, schema) {
+        super()
+        this._connection = connection
+        this._schema = schema
+    }
+    async isConnected() {
+        try {
+            await this._connection.authenticate()
+            return true
+        } catch (error) {
+            console.error('FAIL!', error)
+            return false
+        }
+    }
+    async create(item) {
+        const {
+            dataValues
+        } = await this._schema.create(item)
+
+        return dataValues
+    }
+    async read(item = {}) {
+        return this._schema.findAll({
+            where: item,
+            raw: true
+        })
+    }
+    async update(id, item, upsert=false) {
+        const fn = upsert ? 'upsert' : 'update'
+
+        return this._schema[fn](item, {
+            where: {
+                id
+            }
+        })
+    }
+    async delete(id) {
+        const query = id ? { id } : {}
+        return this._schema.destroy({
+            where: query
+        })
+    }
+    static async defineModel(connection, schema) {
+        const model = await connection.define(schema.name, schema.schema, schema.options)
+        await model.sync()
+        return model
+    }
+    static async connect() {
+        const connection = new Sequelize(process.env.POSTGRES_URL, {
+                quoteIdentifiers: false,
+                operatorsAliases: false,
+                logging: false,
+                ssl: process.env.SSL_DB,
+                dialectOptions: {
+                    ssl: process.env.SSL_DB
+                }
+            }
+        )
+        return connection
+    }
+}
+
+module.exports = Postgres
